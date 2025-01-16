7cf8584a145795c06be480b3675a030534d58ee8
KateSt421 <belporibet@mail.ru>
2025-01-16 22:26:31 +0300

    Обновила пути к картинкам

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ const galleryImage = document.getElementById("galleryImage");
       const nextButton = document.getElementById("nextButton");
 
       prevButton.onclick = () => {
-        galleryImage.src = "/assets/images/cat1.avif";
+        galleryImage.src = "assets/images/cat1.avif";
       };
 
       nextButton.onclick = () => {
-        galleryImage.src = "/assets/images/cat2.avif";
+        galleryImage.src = "assets/images/cat2.avif";
       };
