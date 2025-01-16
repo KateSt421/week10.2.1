e614998057110cf46cd6a5f729d17046e44eb208
KateSt421 <belporibet@mail.ru>
2025-01-16 21:16:43 +0300

    Добавила задание 2 с двумя изображениями

---
+++ script.js
@@ -0,0 +1,20 @@
+let programmingLanguage = 'JavaScript';
+
+function learnLanguage(programmingLanguage) {
+	console.log(`Я учу ${programmingLanguage}!`);
+}
+
+learnLanguage(programmingLanguage);
+
+
+const galleryImage = document.getElementById("galleryImage");
+      const prevButton = document.getElementById("prevButton");
+      const nextButton = document.getElementById("nextButton");
+
+      prevButton.onclick = () => {
+        galleryImage.src = "/assets/images/cat1.avif";
+      };
+
+      nextButton.onclick = () => {
+        galleryImage.src = "/assets/images/cat2.avif";
+      };
